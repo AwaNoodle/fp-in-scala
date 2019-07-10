229a784333c25b56d3c4501f86da69b0adae8aa4
Mark Turner <mark.nick.turner@gmail.com>
2019-07-10 18:47:32 +0100

    Add Remaining MyOption Methods

--- src/main/scala/fp_in_scala/chapter_4/myOption.scala
+++ src/main/scala/fp_in_scala/chapter_4/myOption.scala
@@ -12,9 +12,19 @@ sealed trait MyOption[+A] {
     case MySome(a) => f(a)
   }
 
-  //def getOrElse[B >: A](default: => B): B
-  //def orElse[B >: A](ob: => Option[B]): Option[B]
-  //def filter(f: A => Boolean): Option[A]
+  def getOrElse[B >: A](default: => B): B = this match {
+    case MyNone => default
+    case MySome(a) => a
+  }
+
+  def orElse[B >: A](ob: => MyOption[B]): MyOption[B] = this match {
+    case MyNone => ob
+    case MySome(_) => this
+  }
+
+  def filter(f: A => Boolean): MyOption[A] = {
+    this.flatMap(x => if(f(x) == true) this else MyNone)
+  }
 }
 
 case class MySome[+A](get: A) extends MyOption[A]
--- src/test/scala/fp_in_scala/chapter_4/myOption_spec.scala
+++ src/test/scala/fp_in_scala/chapter_4/myOption_spec.scala
@@ -19,4 +19,21 @@ class myOption_spec extends FlatSpec with Matchers {
     it should "not transform a MyNone via flatmap" in {
       MyNone.flatMap(x => MySome(x.toString())) shouldBe MyNone
     }
+
+    it should "return the wrapped value or a default if MyNone" in {
+      MySome(5).getOrElse(-99) shouldBe 5
+      MyNone.getOrElse(-99) shouldBe -99
+    }
+
+    it should "return the original option is MySome or a replacement MySome if MyNone" in {
+      MySome(5).orElse(MySome(-99)) shouldBe MySome(5)
+      MyNone.orElse(MySome(-99)) shouldBe MySome(-99)
+    }
+
+    it should "convert a MySome to MyNone if the filter predicate fails" in {
+      def test(x: Int) = x == 5
+      MySome(5).filter(test) shouldBe MySome(5)
+      MySome(10).filter(test) shouldBe MyNone
+      MyNone.filter(test) shouldBe MyNone
+    }
 }
