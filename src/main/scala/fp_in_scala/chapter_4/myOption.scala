580c6fcbd49c0c0d58cbdc9a65c32ccf8258743f
Mark Turner <mark.nick.turner@gmail.com>
2019-07-11 09:54:19 +0100

    Update myOption in Terms of Map

--- src/main/scala/fp_in_scala/chapter_4/myOption.scala
+++ src/main/scala/fp_in_scala/chapter_4/myOption.scala
@@ -2,22 +2,21 @@ package fp_in_scala.chapter_4
 
 // 4.1
 sealed trait MyOption[+A] {
-  def map[B](f: A => B): MyOption[B] = this.flatMap(x => MySome(f(x)))
-  
-  def flatMap[B](f: A => MyOption[B]): MyOption[B] = this match {
+  def map[B](f: A => B): MyOption[B] = this match {
     case MyNone => MyNone
-    case MySome(a) => f(a)
+    case MySome(a) => MySome(f(a))
   }
+  
+  def flatMap[B](f: A => MyOption[B]): MyOption[B] = 
+    this.map(f).getOrElse(MyNone)
 
   def getOrElse[B >: A](default: => B): B = this match {
     case MyNone => default
     case MySome(a) => a
   }
 
-  def orElse[B >: A](ob: => MyOption[B]): MyOption[B] = this match {
-    case MyNone => ob
-    case MySome(_) => this
-  }
+  def orElse[B >: A](ob: => MyOption[B]): MyOption[B] = 
+    this.map(MySome(_)).getOrElse(ob)
 
   def filter(f: A => Boolean): MyOption[A] = {
     this.flatMap(x => if(f(x) == true) this else MyNone)
