299fbfb5b2bed47d88d1b750d61c8ddeec62f8ae
Mark Turner <mark.nick.turner@gmail.com>
2019-10-29 16:50:39 +0000

    Add Ex 6.1 Solution

--- src/main/scala/fp_in_scala/chapter_6/RNGOps.scala
+++ src/main/scala/fp_in_scala/chapter_6/RNGOps.scala
@@ -1,5 +1,15 @@
 package fp_in_scala.chapter_6
 
 object RNGOps {
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val next = rng.nextInt
+    
+    if(next._1 == Int.MinValue) {
+      (Int.MaxValue, next._2)
+    } else if(next._1 < 0) {
+      (Math.abs(next._1), next._2)
+    } else {
+      next
+    }
+  }
 }
--- src/test/scala/fp_in_scala/chapter_6/SimpleRNG_spec.scala
+++ src/test/scala/fp_in_scala/chapter_6/SimpleRNG_spec.scala
@@ -13,4 +13,14 @@ class SimpleRNG_spec extends FlatSpec with Matchers {
     val someValue = TestRNG(10)
     nonNegativeInt(someValue) shouldBe (10, someValue)
   }
+
+  it should "return the abs value if the random number is less than 0" in {
+    val someValue = TestRNG(-50)
+    nonNegativeInt(someValue) shouldBe (50, someValue)
+  }
+
+  it should "return Int.MaxValue if Int.MinValue is returned by the random generator" in {
+    val someValue = TestRNG(Int.MinValue)
+    nonNegativeInt(someValue) shouldBe (Int.MaxValue, someValue)
+  }
 }
