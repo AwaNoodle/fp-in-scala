4357cce921dc69832c462517dd78719cea3ced71
Mark Turner <mark.nick.turner@gmail.com>
2019-10-30 18:51:14 +0000

    Add Ex 6.2 & 3 Solution

--- src/main/scala/fp_in_scala/chapter_6/RNGOps.scala
+++ src/main/scala/fp_in_scala/chapter_6/RNGOps.scala
@@ -19,4 +19,32 @@ object RNGOps {
     val nonNeg = nonNegativeInt(rng)
     (nonNeg._1 / (Int.MaxValue.toDouble + 0.1), nonNeg._2)
   }
+
+  // 6.3
+  def intDouble(rng: RNG): ((Int,Double), RNG) = {
+    val (i, iNext) = rng.nextInt
+    val (d, dNext) = double(iNext)
+    ((i, d), dNext)
+  }
+
+  def doubleInt(rng: RNG): ((Double,Int), RNG) = {
+    val ((i,d), next) = intDouble(rng)
+    ((d,i), next)
+  }
+
+  def double3(rng: RNG): ((Double,Double,Double), RNG) = {
+    val (a, aNext) = double(rng)
+    val (b, bNext) = double(aNext)
+    val (c, cNext) = double(bNext)
+
+    ((a,b,c), cNext)
+  }
+
+  // 6.4
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    (1 to count).foldLeft((List[Int](), rng)) { (state, ignore) =>
+      val (i, next) = state._2.nextInt
+      (state._1 :+ i, next)
+    }
+  }
 }
--- src/test/scala/fp_in_scala/chapter_6/RNGOPs_spec.scala
+++ src/test/scala/fp_in_scala/chapter_6/RNGOPs_spec.scala
@@ -9,6 +9,7 @@ class RNGOps_spec extends FlatSpec with Matchers {
     def nextInt: (Int, RNG) = (testValue, this)
   }
 
+  // 6.1
   "nonNegativeInt" should "return an int between 0 and maxValue" in {
     val someValue = TestRNG(10)
     nonNegativeInt(someValue) shouldBe (10, someValue)
@@ -24,6 +25,7 @@ class RNGOps_spec extends FlatSpec with Matchers {
     nonNegativeInt(someValue) shouldBe (Int.MaxValue, someValue)
   }
 
+  // 6.2
   "double" should "generate a random number between 0 and 1" in {
     val someValue = TestRNG(Int.MaxValue / 2)
     double(someValue) shouldBe (0.4999999997438863, someValue)
@@ -32,5 +34,37 @@ class RNGOps_spec extends FlatSpec with Matchers {
   it should "not return 1" in {
     val someValue = TestRNG(Int.MaxValue)
     double(someValue) shouldBe (0.9999999999534339, someValue)
+  }
+
+  // 6.3
+  "intDouble" should "return a random Int,Double pair" in {
+    val halfMaxValue = Int.MaxValue / 2
+    val someValue = TestRNG(halfMaxValue)
+    intDouble(someValue) shouldBe ((halfMaxValue, 0.4999999997438863), someValue)
+  }
+
+  "doubleInt" should "return a random Double,Int pair" in {
+    val halfMaxValue = Int.MaxValue / 2
+    val someValue = TestRNG(halfMaxValue)
+    doubleInt(someValue) shouldBe ((0.4999999997438863, halfMaxValue), someValue)
+  }
+
+  "double3" should "return a tuple3 of random doubles" in {
+    val halfMaxValue = Int.MaxValue / 2
+    val expected = 0.4999999997438863
+    val someValue = TestRNG(halfMaxValue)
+
+    double3(someValue) shouldBe ((expected, expected, expected), someValue)
+  }
+
+  // 6.4
+  "ints" should "return a list of random ints of the desired size" in {
+    val rng = TestRNG(50)
+    val expectedLength = 10
+    val expected = ((1 to expectedLength).map(x => 50), rng)
+    
+    val randomInts = ints(10)(rng) 
+    randomInts._1.length shouldBe expectedLength
+    randomInts shouldBe expected
   }
 }
