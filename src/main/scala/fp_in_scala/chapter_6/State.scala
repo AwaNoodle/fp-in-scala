f4f9f5913ee2cbfa28d6b3f59bedab91551c0c6b
Mark Turner <mark.nick.turner@gmail.com>
2019-11-26 08:12:32 +0000

    Add Sequence and Map2 to State

--- src/main/scala/fp_in_scala/chapter_6/State.scala
+++ src/main/scala/fp_in_scala/chapter_6/State.scala
@@ -16,4 +16,18 @@ final case class State[S, +A](run: S => (A, S)) {
 
 object State {
   def unit[S, A](a: A) = State((s: S) => (a, s))
+
+  def sequence[S,A](fs: List[State[S, A]]): State[S, List[A]] = {
+    fs.foldLeft(State.unit[S, List[A]](Nil)) { (currState, next) =>
+      map2(next, currState)(_ :: _)
+    }
+  }
+
+  def map2[S,A,B,C](sa: State[S, A], sb: State[S,B])(f: (A, B) => C): State[S,C] = {
+    sa.flatMap { a =>
+      sb.map { b => 
+        f(a,b)
+      } 
+    }
+  }
 }
